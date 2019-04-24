5f026a691dde1cb1a8db1d4f92e434bdb0a670a0
Joe Quattrone <joequattrone5@gmail.com>
2019-04-24 16:43:34 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+
   render() {
     return(
       <div>
-        Band Component
+        <li>{this.props.band.name}<button onClick={() => this.props.delete(this.props.band)}>DELETE</button></li>
+        {console.log(this.props.band)}
       </div>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,8 +3,17 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      let band = {
+        id: Math.random()*100000000,
+        name: action.name
+      }
+      return { bands: state.bands.concat(band)}
 
-      return { ...state, bands: [...state.bands, action.name] }
+    case 'DELETE_BAND':
+      let bandToDelete = state.bands.find(band => band.id === action.id)
+
+      return {bands: state.bands.filter(band => band.id !== bandToDelete.id)}
+
 
     default:
       return state;
